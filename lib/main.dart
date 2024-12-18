653a8e91110c2210f65839a4e11aa4defa6f1a26
Muhammad Amir Syafiq <116126865+AmirLast@users.noreply.github.com>
2024-12-18 12:12:24 +0800

    aight

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,20 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:fyp/services/auth/auth_gate.dart';
 import 'package:fyp/firebase_options.dart';
-//import 'package:provider/provider.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(
-    /*MultiProvider(
+    MultiProvider(
       providers: const [
         //shop provider
         //ChangeNotifierProvider(create: (context) => Shop()),
       ],
       child: const MyApp(),
-    ),*/
-    const MyApp(),
+    ),
   );
 }
 
